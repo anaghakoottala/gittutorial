41405c4fcfa81643f4d86f2a20a016ee38723193
user <user@fisat>
2018-04-18 04:47:44 -0400

    create a function to add two numbers

--- add.py
+++ add.py
@@ -2,7 +2,8 @@ def sum(a,b):
 	s=a+b;
 	return s;
 
-a=input("Enter a number: ");
-b=input("Enter next number: ");
-sum(a,b);
+#a=input("Enter a number: ");
+#b=input("Enter next number: ");
+#sum(a,b);
+print sum(5,6);
 
